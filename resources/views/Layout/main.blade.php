e2fab54adc6881564e861fe8f8dd9af28dc233eb
Amanda <arpuga95@gmail.com>
2017-09-03 09:43:44 -0300

    Form incompleto

--- resources/views/Layout/main.blade.php
+++ resources/views/Layout/main.blade.php
@@ -51,7 +51,11 @@
                         <div class="form-group">
                             <input type="text" class="form-control busca" placeholder="Busca">
                         </div>
-                        <button type="submit" class="btn btn-default busca">Buscar</button>
+                        <button type="submit" class="btn btn-default busca">
+                        <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+                        </button>
+                        
+                        
                     </form>
 
                    <ul class="nav navbar-nav navbar-right"> 
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,7 +3,108 @@
 
 @section('content')
 
-<h1>EXEMPLO CORPO DE LOGIN E CADASTRO</h1>
 
+<script>
+    function pessoa(tipo){
+      if(tipo=="fisica"){
+      document.getElementById("fisica").style.display = "inline";
+      document.getElementById("juridica").style.display = "none";
+      }else if(tipo=="juridica"){
+      document.getElementById("fisica").style.display = "none";
+      document.getElementById("juridica").style.display = "inline";
+
+      }
+
+    }
+  </script>
+
+<style type="text/css">
+#form-cadas{
+    font-family: 'Acme';
+    position: relative;
+    float: left;
+    width: 300px;
+    height: 270px;
+    }
+#form-log{
+    font-family: 'Acme';
+    position: relative;
+    width: 300px;
+    height: 270px;
+}
+legend
+{
+    font-style:bold;
+    font-family: 'Acme';
+    text-align: center;
+}
+#form label
+{
+   
+}
+
+h1{   
+    color: orange;
+   font-family: 'Acme';
+   text-align:  center;
+}
+
+</style>
+
+<h1>Venha se mover!</h1>
+
+
+ <div id="form-cadas" class="form-group"  style="text-align:center; margin-left: 300px ;">
+     
+      <h3>Cadastre-se!</h3>
+          <p><input type="radio" name="optradio" value="juridica" onclick="pessoa(this.value);">Pessoa Juridica
+          <input class="active" type="radio" name="optradio" value="fisica" onclick="pessoa(this.value);">Pessoa Fisica
+          </p>
+
+
+        <form id="juridica" style="display:none;">
+          <fieldset>
+             
+              <label>Nome Fantasia: </label><input type="text" name="nomeFan"></br></br>
+              <label>CNPJ: </label><input type="text" name="cnpj"></br></br>
+              <label>Endereço: </label><input type="text" name="endereço"></br></br>
+              <label>Email: </label><input type="text" name="email"></br></br>
+              <label>Senha: </label><input type="text" name="senha"></br></br>
+            <input class="btn_submit" type="submit" name="Cadastrar">
+
+
+        </fieldset>
+          </form>
+
+          <form id="fisica">
+          <fieldset>
+              
+              <label>Email: </label><input type="text" class="email"></br></br>
+              <label>CPF: </label><input type="text" class="cpf"></br></br>
+            <input class="btn_submit" type="submit" value="Cadastrar">
+
+
+        </fieldset>
+          </form>
+
+</div>
+
+<hr>
+
+   
+
+    <div id="form-cadas" class="form-group"  style="text-align:center; margin: 0 auto;">
+        <h3>Login!</h3>
+            <form>
+             <fieldset>
+              
+              <label>Email: </label><input type="text" class="email"></br></br>
+              <label>Senha: </label><input type="text" class="senha"></br></br>
+            <input class="btn_submit" type="submit" value="Entrar">
+
+
+        </fieldset>
+          </form>
+    </div>
 
 @endsection
