23aaa20a72acece69e00b3ac1a80d1ab8d3e15cb
Amanda <arpuga95@gmail.com>
2018-04-16 20:44:13 -0300

    Auto stash before merge of "frontend" and "origin/frontend"
    Favoritos (fazendo)

--- resources/views/temp/favoritos.blade.php
+++ resources/views/temp/favoritos.blade.php
@@ -1,8 +1,68 @@
 @extends('layout.main')
-@section('title','Contato')
+@section('title','Favoritos')
 
 @section('content')
 
+    <link rel="stylesheet" type="text/css" href="/css/app.css">
+    <link rel="stylesheet" type="text/css" href="/css/style.css">
+
+
+
+    <div class="container">
+      <div class="section">
+
+
+ <div class="row">
+  <div class="col-xs-12 col-sm-6">
+    <div class="card">
+    	<img class="card-img-top" src="/img/perfil.jpg" style="width:150px">
+      <div class="card-body">
+        <h5 class="card-title">Nome Fantasia</h5>
+        <p class="card-text">Endereço, telefone e pequena descrição.</p>
+        <a href="#" class="btn btn-primary">Visitar</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-xs-12 col-sm-6">
+    <div class="card">
+    	<img class="card-img-top" src="/img/perfil.jpg" style="width:150px">
+      <div class="card-body">
+        <h5 class="card-title">Nome Fantasia</h5>
+        <p class="card-text">Endereço, telefone e pequena descrição.</p>
+        <a href="#" class="btn btn-primary">Visitar</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+ <div class="row">
+  <div class="col-xs-12 col-sm-6">
+     <div class="card">
+    	<img class="card-img-top" src="/img/perfil.jpg" style="width:150px">
+      <div class="card-body">
+        <h5 class="card-title">Nome Fantasia</h5>
+        <p class="card-text">Endereço, telefone e pequena descrição.</p>
+        <a href="#" class="btn btn-primary">Visitar</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-xs-12 col-sm-6">
+     <div class="card">
+    	<img class="card-img-top" src="/img/perfil.jpg" style="width:150px">
+      <div class="card-body">
+        <h5 class="card-title">Nome Fantasia</h5>
+        <p class="card-text">Endereço, telefone e pequena descrição.</p>
+        <a href="#" class="btn btn-primary">Visitar</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+     </div>
+	</div>
+ 
 
 
 @endsection
