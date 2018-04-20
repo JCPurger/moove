0745259e727e963899042231a56aa9f347d28253
Amanda <arpuga95@gmail.com>
2018-04-20 09:16:48 -0300

    Favoritos
    completo. eu acho

--- resources/views/temp/favoritos.blade.php
+++ resources/views/temp/favoritos.blade.php
@@ -10,55 +10,55 @@
 
     <div class="container">
       <div class="section">
+<h1>Seus lugares favoritos estão aqui!</h1>
 
+ <div class="row row-margin-bottom">
+            <div class="col-md-5 no-padding lib-item" data-category="view">
+                <div class="lib-panel">
+                    <div class="row box-shadow">
+                        <div class="col-md-6">
+                            <img class="lib-img-show" src="/img/perfil.jpg" style="width: 230px">
+                        </div>
+                        <div class="col-md-6">
+                            <div class="lib-row lib-header">
+                                Nome Empresa
+                                <div class="lib-header-seperator"></div>
+                            </div>
+                            <div class="lib-row lib-desc">
+                                   <p class="card-text">A sessão Descrição da Empresa do plano de negócios deve apresentar um breve resumo da organização da sua empresa ou negócio, sua história, etc.</p>
+                                    <a href="#" class="btn btn-primary btn-sm">Ver mais</a>
+                                    <a href="#" class="btn btn-danger btn-sm">Excluir</i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-1"></div>
+            <div class="col-md-5 no-padding lib-item" data-category="ui">
+                <div class="lib-panel">
+                    <div class="row box-shadow">
+                        <div class="col-md-6">
+                            <img class="lib-img" src="/img/perfil.jpg" style="width: 230px">
+                        </div>
+                        <div class="col-md-6">
+                            <div class="lib-row lib-header">
+                                Nome Empresa
+                                <div class="lib-header-seperator"></div>
+                            </div>
+                            <div class="lib-row lib-desc">
+                                   <p class="card-text">A sessão Descrição da Empresa do plano de negócios deve apresentar um breve resumo da organização da sua empresa ou negócio, sua história, etc.</p>
+                                    <a href="#" class="btn btn-primary btn-sm">Ver mais</a>
+                                    <a href="#" class="btn btn-danger btn-sm">Excluir</i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
- <div class="row">
-  <div class="col-xs-12 col-sm-6">
-    <div class="card">
-    	<img class="card-img-top" src="/img/perfil.jpg" style="width:150px">
-      <div class="card-body">
-        <h5 class="card-title">Nome Fantasia</h5>
-        <p class="card-text">Endereço, telefone e pequena descrição.</p>
-        <a href="#" class="btn btn-primary">Visitar</a>
-      </div>
-    </div>
-  </div>
-  <div class="col-xs-12 col-sm-6">
-    <div class="card">
-    	<img class="card-img-top" src="/img/perfil.jpg" style="width:150px">
-      <div class="card-body">
-        <h5 class="card-title">Nome Fantasia</h5>
-        <p class="card-text">Endereço, telefone e pequena descrição.</p>
-        <a href="#" class="btn btn-primary">Visitar</a>
-      </div>
-    </div>
-  </div>
+<div>
+	<h3>Poxa! Você ainda não tem favoritos <img src="img/triste.jpg" style="height:35px"></h3>
 </div>
-
-
- <div class="row">
-  <div class="col-xs-12 col-sm-6">
-     <div class="card">
-    	<img class="card-img-top" src="/img/perfil.jpg" style="width:150px">
-      <div class="card-body">
-        <h5 class="card-title">Nome Fantasia</h5>
-        <p class="card-text">Endereço, telefone e pequena descrição.</p>
-        <a href="#" class="btn btn-primary">Visitar</a>
-      </div>
-    </div>
-  </div>
-  <div class="col-xs-12 col-sm-6">
-     <div class="card">
-    	<img class="card-img-top" src="/img/perfil.jpg" style="width:150px">
-      <div class="card-body">
-        <h5 class="card-title">Nome Fantasia</h5>
-        <p class="card-text">Endereço, telefone e pequena descrição.</p>
-        <a href="#" class="btn btn-primary">Visitar</a>
-      </div>
-    </div>
-  </div>
-</div>
-
 
      </div>
 	</div>
