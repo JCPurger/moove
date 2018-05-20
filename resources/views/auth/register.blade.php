08fa9fb0078ff67b34e93724396f9956f73952fc
Unknown <jc.purger@gmail.com>
2018-05-20 17:50:40 -0300

    botão de tipo de usuário em registro

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,7 +8,6 @@
 <!--Cadastro -->
 <div id="form-cadas" class="container-fluid"  style="text-align:center; margin: 0 auto;">
 
-
     <div class="container">
         <div class="row">
             <div class="col-md-6 col-md-offset-3">
@@ -25,9 +24,9 @@
                         <hr>
                     </div>
 
-                    <div class="col-lg-12 well">
+                    <div class="col-lg-12 well" id="usuario-form" >
                         <!-- Cadastro Usuario -->
-                        <form id="usuario-form" action="#" method="post" role="form" style="display: none;">
+                        <form action="#" method="post" role="form">
                             <div class="col-sm-12">
                                 <div class="row">
                                     <div class="col-sm-6 form-group">
@@ -38,12 +37,12 @@
                                         <label>Último Nome</label>
                                         <input type="text" id="sobrenome" placeholder="Enter Last Name Here.." class="form-control">
                                     </div>
-                                </div>                    
-                                <div class="row">                     
+                                </div>
+                                <div class="row">
                                     <div class="form-group">
                                         <label>Email</label>
                                         <input type="text" id="email" placeholder="Enter Eamil Here.." class="form-control">
-                                    </div>      
+                                    </div>
                                     <div class="row">
                                         <div class="col-sm-6 form-group">
                                             <label>Senha</label>
@@ -53,21 +52,21 @@
                                             <label>Confirmação de Senha</label>
                                             <input type="password" id="confirmsenha" placeholder="Enter Password Here.." class="form-control">
                                         </div>
-                                    </div> 
+                                    </div>
                                     <div class="form-group">
                                         <label>Foto de Perfil</label>
                                         <input type="file" id="perfil" class="form-control">
                                     </div>
-                                    <button type="button" class="btn btn-lg btn-info">Cadastrar</button> 
-                                </div>                  
+                                    <button type="button" class="btn btn-lg btn-info">Cadastrar</button>
+                                </div>
                             </div>
-                        </form> 
+                        </form>
 
                     </div>
                     <!-- Cadastro Empresa -->
-                    <div class="col-lg-12 well">
+                    <div class="col-lg-12 well" id="empresa-form" style="display: none;">
                         <div class="row">
-                            <form id="register-form" action="http://phpoll.com/register/process" method="post" role="form" style="display: block;">
+                            <form  action="http://phpoll.com/register/process" method="post" role="form" style="display: block;">
                                 <div class="col-sm-12">
                                     <div class="row">
                                         <div class="col-sm-6 form-group">
@@ -78,20 +77,20 @@
                                             <label>CNPJ</label>
                                             <input type="text" id="cnpj" placeholder="Enter CNPJ Here.." class="form-control">
                                         </div>
-                                    </div>                  
+                                    </div>
                                     <div class="form-group">
                                         <label>Endereço</label>
                                         <input type="text "id="endereco" placeholder="Enter Endereco Here.." class="form-control">
-                                    </div>  
+                                    </div>
                                     <div class="row">
                                         <div class="col-sm-6 form-group">
                                             <label>Número</label>
                                             <input type="text" id="numero" placeholder="Enter Number Here.." class="form-control">
-                                        </div>  
+                                        </div>
                                         <div class="col-sm-6 form-group">
                                             <label>Complemento</label>
                                             <input type="text" id="complemento" placeholder="Enter Complement Here.." class="form-control">
-                                        </div>  
+                                        </div>
                                         <div class="col-sm-12 form-group">
                                             <label>Fotos</label>
                                             <input type="file" id="fotos" class="form-control">
@@ -100,12 +99,12 @@
                                             <div class="col-sm-6 form-group">
                                                 <label>Telefone</label>
                                                 <input type="text" id="tel" placeholder="Enter Telefone .." class="form-control">
-                                            </div>      
+                                            </div>
                                             <div class="col-sm-6 form-group">
                                                 <label>Email</label>
                                                 <input type="text" id="emailEmp" placeholder="Enter Company Name Here.." class="form-control">
-                                            </div>  
-                                        </div>                       
+                                            </div>
+                                        </div>
                                         <div class="row">
                                             <div class="col-sm-6 form-group">
                                                 <label>Senha</label>
@@ -115,42 +114,43 @@
                                                 <label>Confirmação de Senha</label>
                                                 <input type="password" id="confirmsenhaEmp" placeholder="Enter Password Here.." class="form-control">
                                             </div>
-                                        </div> 
+                                        </div>
                                         <div class="form-group">
                                             <label>Website</label>
                                             <input type="text" id="site" placeholder="Enter Website Name Here.." class="form-control">
                                         </div>
                                         <button type="button" class="btn btn-lg btn-info">Cadastrar</button>
-                                    </div>                  
+                                    </div>
                                 </div>
-                            </form> 
+                            </form>
                         </div>
                     </div>
 
                 </div>
 
-               
+
 
  @endsection
 
 @section("scripts")
 <script>
 $(function() {
-
-$('#usuario-form-link').click(function(e) {
-$("#usuario-form").delay(100).fadeIn(100);
-$("#empresa-form").fadeOut(100);
-$('#empresa-form-link').removeClass('active');
-$(this).addClass('active');
-e.preventDefault();
-});
-$('#empresa-form-link').click(function(e) {
-$("#empresa-form").delay(100).fadeIn(100);
-$("#usuario-form").fadeOut(100);
-$('#usuario-form-link').removeClass('active');
-$(this).addClass('active');
-e.preventDefault();
+    $('#usuario-form-link').click(function(e) {
+        console.log('usuario click !');
+        e.preventDefault();
+        $("#usuario-form").delay(100).fadeIn(100);
+        $("#empresa-form").fadeOut(100);
+        $('#empresa-form-link').removeClass('active');
+        $(this).addClass('active');
+    });
+    $('#empresa-form-link').click(function(e) {
+        console.log('empresa click !');
+        e.preventDefault();
+        $("#empresa-form").delay(100).fadeIn(100);
+        $("#usuario-form").fadeOut(100);
+        $('#usuario-form-link').removeClass('active');
+        $(this).addClass('active');
+    })
 });
 </script>
 @endsection
-});
