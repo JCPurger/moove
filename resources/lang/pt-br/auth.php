3e1e28b5b2e1519dd2f9842c313af022ea78ec0a
Unknown <jc.purger@gmail.com>
2018-06-04 00:17:15 -0300

    tradução de msg de erro para cadastro

--- resources/lang/pt-br/auth.php
+++ resources/lang/pt-br/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Suas credencias não correspondem com as registradas.',
+    'throttle' => 'Muitas tentativas de login em pouco tempo. Porfavor tente novamente daqui a :seconds segundos.',
 
 ];
