d2bd1111f35a418914589f9e61fb0080a38478ce
Unknown <jc.purger@gmail.com>
2018-07-02 00:12:02 -0300

    string limit para descricao em rank

--- resources/views/ranking.blade.php
+++ resources/views/ranking.blade.php
@@ -35,7 +35,7 @@
                                     </td>
                                     <td>{{ $place->nome }}</td>
                                     <td>{{ $place->category()->first()->nome }}</td>
-                                    <td>{{ $place->descricao }}</td>
+                                    <td>{{ str_limit($place->descricao,60) }}</td>
                                 </tr>
                             @endforeach
                             </tbody>
